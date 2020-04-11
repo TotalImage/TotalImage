2db58812fd0b0a6dbff95b480cefa04a8be7e187
David Hrdlička <dhrdlicka@users.noreply.github.com>
2020-04-11 02:29:07 +0200

    Added some LINQ black magic to the directory class

--- TotalImage/FileSystems/Directory.cs
+++ TotalImage/FileSystems/Directory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using static System.IO.Path;
 
 namespace TotalImage.FileSystems
@@ -11,9 +12,26 @@ namespace TotalImage.FileSystems
         public abstract Directory Parent { get; }
         public abstract Directory Root { get; }
 
-        public abstract Directory[] GetDirectories();
-        public abstract File[] GetFiles();
-        public abstract FileSystemObject[] GetFileSystemObjects();
+        public abstract IEnumerable<FileSystemObject> EnumerateFileSystemObjects();
+
+        public IEnumerable<Directory> EnumerateDirectories()
+            => from x in EnumerateFileSystemObjects()
+                where x is Directory
+                select x as Directory;
+
+        public IEnumerable<File> EnumerateFiles()
+            => from x in EnumerateFileSystemObjects()
+                where x is File
+                select x as File;
+
+        public FileSystemObject[] GetFileSystemObjects()
+            => EnumerateFileSystemObjects().ToArray();
+
+        public Directory[] GetDirectories()
+            => EnumerateDirectories().ToArray();
+
+        public File[] GetFiles()
+            => EnumerateFiles().ToArray();
 
         public override string FullName
         {
