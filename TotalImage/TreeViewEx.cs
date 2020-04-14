56b37e34b8444243d1321c2898c897f38e3dfa5d
David Hrdlička <dhrdlicka@users.noreply.github.com>
2020-04-14 19:12:51 +0200

    Add treeview constants to Interop

--- TotalImage/TreeViewEx.cs
+++ TotalImage/TreeViewEx.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using static Interop.ComCtl32;
+using static Interop.User32;
 using static Interop.UxTheme;
 
 namespace TotalImage
 {
     public class TreeViewEx : TreeView
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
-
         protected override void CreateHandle()
         {
             base.CreateHandle();
             SetWindowTheme(this.Handle, "explorer", null); //Enable Explorer-like appearance
-            SendMessage(this.Handle, 0x1100 + 44, 0x0020, 0x0020); //Enable auto-scroll extended style
+            SendMessage(this.Handle, (uint)TVM.SETEXTENDEDSTYLE, (IntPtr)TVS_EX.AUTOHSCROLL, (IntPtr)TVS_EX.AUTOHSCROLL); //Enable auto-scroll extended style
         }
     }
 }
