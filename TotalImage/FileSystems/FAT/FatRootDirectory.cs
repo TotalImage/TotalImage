9e239a265d5eb8bdfcbc7681152ab113b6cc776b
David Hrdlička <dhrdlicka@users.noreply.github.com>
2020-08-01 22:14:53 +0200

    Fix FAT objects including . and .. subdirs

--- TotalImage/FileSystems/FAT/FatRootDirectory.cs
+++ TotalImage/FileSystems/FAT/FatRootDirectory.cs
@@ -34,9 +34,10 @@ namespace TotalImage.FileSystems.FAT
                     byte firstByte = reader.ReadByte();
 
                     /* 0x00/0xF6 = no more entries after this one, stop
-                     * 0xE5/0x05 = deleted entry, skip for now */
+                     * 0xE5/0x05 = deleted entry, skip for now 
+                     * 0x2E      = virtual . and .. folders, skip*/
                     if (firstByte == 0x00 || firstByte == 0xF6) break;
-                    else if (firstByte == 0xE5 || firstByte == 0x05) continue;
+                    else if (firstByte == 0xE5 || firstByte == 0x05 || firstByte == 0x2E) continue;
 
                     stream.Seek(-0x01, SeekOrigin.Current);
                     var entry = DirectoryEntry.Parse(reader.ReadBytes(32));
