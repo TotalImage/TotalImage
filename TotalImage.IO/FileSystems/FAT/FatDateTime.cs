8d69efc17cad20cd1b624b3fb834881d9d1ba327
David Hrdlička <dhrdlicka@users.noreply.github.com>
2021-04-30 20:38:05 +0200

    FatDateTime fixes

--- TotalImage.IO/FileSystems/FAT/FatDateTime.cs
+++ TotalImage.IO/FileSystems/FAT/FatDateTime.cs
@@ -13,6 +13,9 @@ namespace TotalImage.FileSystems.FAT
             return (year, month, day);
         }
 
+        public static ushort PackFatDate(int year, int month, int day)
+            => (ushort)(((year - 1980) << 9) | (month << 5) | day);
+
         public static (int, int, int) UnpackFatTime(ushort time)
         {
             var hour = (time & 0xF800) >> 11;
@@ -21,6 +24,9 @@ namespace TotalImage.FileSystems.FAT
 
             return (hour, minute, second);
         }
+
+        public static ushort PackFatTime(int hour, int minute, int second)
+            => (ushort)((hour << 11) | (minute << 5) | (second >> 1));
 
         static int[] dayCounts = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -63,16 +69,19 @@ namespace TotalImage.FileSystems.FAT
 
         public static DateTime? ToDateTime(ushort date, ushort time, byte csec)
             => csec < 200 ? ToDateTime(date, time)?.AddMilliseconds(csec * 10) : null;
+    }
 
+    internal static class DateTimeExtensions
+    {
         public static (ushort, ushort, byte) ToFatDateTime(this DateTime dateTime)
         {
             if (dateTime.Year < 1980) throw new InvalidOperationException();
 
-            var date = ((dateTime.Year - 1980) << 9) | (dateTime.Month << 5) | dateTime.Day;
-            var time = (dateTime.Hour << 11) | (dateTime.Minute << 5) | (dateTime.Second >> 1);
-            var msec = ((dateTime.Second & 1) * 100) + (dateTime.Millisecond / 100);
+            var date = FatDateTime.PackFatDate(dateTime.Year, dateTime.Month, dateTime.Day);
+            var time = FatDateTime.PackFatTime(dateTime.Hour, dateTime.Minute, dateTime.Second);
+            var msec = (byte)((dateTime.Second & 1) * 100 + dateTime.Millisecond / 10);
 
-            return ((ushort)date, (ushort)time, (byte)msec);
+            return (date, time, msec);
         }
     }
 }
