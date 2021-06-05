e3ea18650eb61400e911d7bd5f238800fe2f31a8
David Simunič <simunic.david@outlook.com>
2021-06-05 19:36:01 +0200

    Better solution for previous commit

--- TotalImage/frmMain.cs
+++ TotalImage/frmMain.cs
@@ -227,7 +227,7 @@ namespace TotalImage
 
                 //Create a new image and immediately open it
                 image = RawContainer.CreateImage(bpb, dlg.Geometry.Tracks, dlg.WriteBPB);
-                OpenImage();
+                OpenImage(null);
             }
         }
 
@@ -1578,89 +1578,28 @@ namespace TotalImage
             return sb.ToString();
         }
 
-        //For opening a newly created image that's not yet saved to disk
-        private void OpenImage()
+        //Opens an image
+        private void OpenImage(string? path)
         {
-            CurrentPartitionIndex = 0;
-            if (image.PartitionTable.Partitions.Count == 0)
+            //Opening an existing file, otherwise it's a newly created image
+            if (path != null)
             {
-                TaskDialog.ShowDialog(this, new TaskDialogPage()
+                filepath = path;
+                filename = Path.GetFileName(path);
+                Text = $"{filename} - TotalImage";
+
+                bool memoryMapping = new FileInfo(path).Length > Settings.CurrentSettings.MemoryMappingThreshold;
+
+                var ext = Path.GetExtension(filename).ToLowerInvariant();
+                switch (ext)
                 {
-                    Text = $"We couldn't find any partitions in your image. If this is a hard disk image, verify that it's partitioned with either MBR or GPT partitioning scheme and contains at least one partition.{Environment.NewLine}{Environment.NewLine}" +
-                    $"If you think this is a bug, please submit a bug report (with this image included) on our GitHub repo.",
-                    Heading = "No partitions detected",
-                    Caption = "Error",
-                    Buttons =
-                        {
-                            TaskDialogButton.OK
-                        },
-                    Icon = TaskDialogIcon.Error,
-                });
-
-                CloseImage();
-                return;
-            }
-            else if (image.PartitionTable.Partitions.Count >= 1)
-            {
-                if (image.PartitionTable.Partitions.Count > 1)
-                {
-                    dlgSelectPartition selectFrm = new dlgSelectPartition()
-                    {
-                        PartitionTable = image.PartitionTable
-                    };
-
-                    if (selectFrm.ShowDialog() == DialogResult.Cancel)
-                    {
-                        CloseImage();
-                        return;
-                    }
-
-                    CurrentPartitionIndex = selectFrm.SelectedEntry;
+                    case ".vhd":
+                        image = new VhdContainer(path, memoryMapping);
+                        break;
+                    default:
+                        image = new RawContainer(path, memoryMapping);
+                        break;
                 }
-
-                selectPartitionToolStripComboBox.Items.Clear();
-
-                if (image.PartitionTable is not Partitions.NoPartitionTable)
-                {
-                    for (int i = 0; i < image.PartitionTable.Partitions.Count; i++)
-                    {
-                        try
-                        {
-                            selectPartitionToolStripComboBox.Items.Add($"{i}: {image.PartitionTable.Partitions[i].FileSystem.VolumeLabel.TrimEnd(' ')} ({image.PartitionTable.Partitions[i].FileSystem.DisplayName}, {Settings.CurrentSettings.SizeUnit.FormatSize((ulong)image.PartitionTable.Partitions[i].Length)})");
-                        }
-                        catch (InvalidDataException)
-                        {
-                        }
-
-                        if (i == CurrentPartitionIndex)
-                        {
-                            selectPartitionToolStripComboBox.SelectedIndex = i;
-                        }
-                    }
-                }
-            }
-
-            LoadPartitionInCurrentImage(CurrentPartitionIndex);
-        }
-
-        //TODO: This needs some serious rethinking and probably restructuring.
-        private void OpenImage(string path)
-        {
-            filepath = path;
-            filename = Path.GetFileName(path);
-            Text = $"{filename} - TotalImage";
-
-            bool memoryMapping = new FileInfo(path).Length > Settings.CurrentSettings.MemoryMappingThreshold;
-
-            var ext = Path.GetExtension(filename).ToLowerInvariant();
-            switch (ext)
-            {
-                case ".vhd":
-                    image = new VhdContainer(path, memoryMapping);
-                    break;
-                default:
-                    image = new RawContainer(path, memoryMapping);
-                    break;
             }
 
             CurrentPartitionIndex = 0;
