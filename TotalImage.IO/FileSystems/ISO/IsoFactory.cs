77251052d24a02291e3863c63056e64a4ec6dde7
Thomas Hounsell <commits@thounsell.co.uk>
2021-07-11 09:08:26 +0100

    ISO9660: Fix loading images with leading empty sectors

--- TotalImage.IO/FileSystems/ISO/IsoFactory.cs
+++ TotalImage.IO/FileSystems/ISO/IsoFactory.cs
@@ -12,13 +12,27 @@ namespace TotalImage.FileSystems.ISO
         /// <inheritdoc />
         public FileSystem? TryLoadFileSystem(Stream stream)
         {
-            stream.Seek(0x8001, SeekOrigin.Begin);
+            long nextOffset = 0x8001;
             byte[] identifier = new byte[5];
-            stream.Read(identifier);
-            if (identifier.SequenceEqual(IsoVolumeDescriptor.StandardIdentifier))
+
+            do
             {
-                return new Iso9660FileSystem(stream);
+                stream.Seek(nextOffset, SeekOrigin.Begin);
+                stream.Read(identifier);
+
+                if (identifier.SequenceEqual(IsoVolumeDescriptor.StandardIdentifier))
+                {
+                    // trim off any leading blocks in this image
+
+                    var partialStreamStart = nextOffset - 0x8001;
+                    return partialStreamStart == 0
+                        ? new Iso9660FileSystem(stream)
+                        : new Iso9660FileSystem(new PartialStream(stream, partialStreamStart, stream.Length - partialStreamStart));
+                }
+
+                nextOffset += 0x800;
             }
+            while (stream.Length > nextOffset);
 
             return null;
         }
