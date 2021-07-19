37f3b0fd992c35d2b0b7821147e19892533d17ed
David Simunič <simunic.david@outlook.com>
2021-07-19 18:09:19 +0200

    Image info dialog fixes
    Fixed size formatting so bytes aren't displayed twice if bytes are the preferred unit
    Now showing the number of subdirectories and files in the image
    Now showing "N/A" when volume label is whitespace
    Now setting right column width to max possible without introducing horizontal scrollbar
    Other minor tweaks of placeholder values

--- TotalImage/dlgImageInfo.Designer.cs
+++ TotalImage/dlgImageInfo.Designer.cs
@@ -33,7 +33,7 @@
             System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("File system information", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("Miscellaneous", System.Windows.Forms.HorizontalAlignment.Left);
             System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Filename",
+            "File name",
             "<filename>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
             "File size",
@@ -57,7 +57,7 @@
             "Selected partition",
             "<selectedpartition>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Selected partition ID",
+            "Partition ID/type",
             "<selectedpartitionID>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem(new string[] {
             "File system",
@@ -66,21 +66,24 @@
             "Volume label",
             "<volumelabel>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem12 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Volume serial number",
+            "<volumeserial>"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem(new string[] {
             "Total storage capacity",
             "<capacity>"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem(new string[] {
+            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem(new string[] {
             "Free space",
             "<freespace>"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem(new string[] {
-            "No. of files",
-            "<nofiles>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem(new string[] {
-            "No. of subdirectories",
-            "<subdirectories>"}, -1);
+            "Files",
+            "<nofiles>"}, -1);
             System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Subdirectories",
+            "<subdirectories>"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem(new string[] {
             "MD5 hash",
             "<md5hash>"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem(new string[] {
+            System.Windows.Forms.ListViewItem listViewItem18 = new System.Windows.Forms.ListViewItem(new string[] {
             "SHA-1 hash",
             "<sha1hash>"}, -1);
             this.btnOK = new System.Windows.Forms.Button();
@@ -98,9 +101,10 @@
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnOK.Location = new System.Drawing.Point(381, 12);
+            this.btnOK.Location = new System.Drawing.Point(476, 15);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(80, 26);
+            this.btnOK.Size = new System.Drawing.Size(100, 32);
             this.btnOK.TabIndex = 0;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -110,27 +114,30 @@
             this.pnlBottom.BackColor = System.Drawing.SystemColors.Control;
             this.pnlBottom.Controls.Add(this.btnOK);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 393);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 492);
+            this.pnlBottom.Margin = new System.Windows.Forms.Padding(4);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(473, 50);
+            this.pnlBottom.Size = new System.Drawing.Size(591, 62);
             this.pnlBottom.TabIndex = 22;
             // 
             // lblComment
             // 
             this.lblComment.AutoSize = true;
-            this.lblComment.Location = new System.Drawing.Point(12, 269);
+            this.lblComment.Location = new System.Drawing.Point(15, 336);
+            this.lblComment.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblComment.Name = "lblComment";
-            this.lblComment.Size = new System.Drawing.Size(64, 15);
+            this.lblComment.Size = new System.Drawing.Size(77, 20);
             this.lblComment.TabIndex = 23;
             this.lblComment.Text = "Comment:";
             // 
             // txtComment
             // 
-            this.txtComment.Location = new System.Drawing.Point(12, 287);
+            this.txtComment.Location = new System.Drawing.Point(15, 359);
+            this.txtComment.Margin = new System.Windows.Forms.Padding(4);
             this.txtComment.Multiline = true;
             this.txtComment.Name = "txtComment";
             this.txtComment.ReadOnly = true;
-            this.txtComment.Size = new System.Drawing.Size(449, 91);
+            this.txtComment.Size = new System.Drawing.Size(560, 113);
             this.txtComment.TabIndex = 24;
             // 
             // lstProperties
@@ -168,7 +175,7 @@
             this.lstProperties.HideSelection = false;
             listViewItem1.Group = listViewGroup1;
             listViewItem1.StateImageIndex = 0;
-            listViewItem1.Tag = "Filename";
+            listViewItem1.Tag = "File name";
             listViewItem2.Group = listViewGroup1;
             listViewItem2.StateImageIndex = 0;
             listViewItem2.Tag = "Filesize";
@@ -187,26 +194,29 @@
             listViewItem8.StateImageIndex = 0;
             listViewItem8.Tag = "Selpart";
             listViewItem9.Group = listViewGroup2;
+            listViewItem9.Tag = "PartID";
             listViewItem10.Group = listViewGroup3;
             listViewItem10.StateImageIndex = 0;
             listViewItem10.Tag = "Fs";
             listViewItem11.Group = listViewGroup3;
             listViewItem12.Group = listViewGroup3;
-            listViewItem12.StateImageIndex = 0;
-            listViewItem12.Tag = "Capacity";
+            listViewItem12.Tag = "VolSerial";
             listViewItem13.Group = listViewGroup3;
             listViewItem13.StateImageIndex = 0;
-            listViewItem13.Tag = "Freespace";
+            listViewItem13.Tag = "Capacity";
             listViewItem14.Group = listViewGroup3;
             listViewItem14.StateImageIndex = 0;
-            listViewItem14.Tag = "Filecount";
+            listViewItem14.Tag = "Freespace";
             listViewItem15.Group = listViewGroup3;
             listViewItem15.StateImageIndex = 0;
-            listViewItem15.Tag = "Dircount";
-            listViewItem16.Group = listViewGroup4;
-            listViewItem16.Tag = "md5";
+            listViewItem15.Tag = "Filecount";
+            listViewItem16.Group = listViewGroup3;
+            listViewItem16.StateImageIndex = 0;
+            listViewItem16.Tag = "Dircount";
             listViewItem17.Group = listViewGroup4;
-            listViewItem17.Tag = "sha1";
+            listViewItem17.Tag = "md5";
+            listViewItem18.Group = listViewGroup4;
+            listViewItem18.Tag = "sha1";
             this.lstProperties.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
             listViewItem1,
             listViewItem2,
@@ -224,12 +234,14 @@
             listViewItem14,
             listViewItem15,
             listViewItem16,
-            listViewItem17});
+            listViewItem17,
+            listViewItem18});
             this.lstProperties.LabelWrap = false;
-            this.lstProperties.Location = new System.Drawing.Point(12, 12);
+            this.lstProperties.Location = new System.Drawing.Point(15, 15);
+            this.lstProperties.Margin = new System.Windows.Forms.Padding(4);
             this.lstProperties.MultiSelect = false;
             this.lstProperties.Name = "lstProperties";
-            this.lstProperties.Size = new System.Drawing.Size(449, 245);
+            this.lstProperties.Size = new System.Drawing.Size(560, 305);
             this.lstProperties.TabIndex = 26;
             this.lstProperties.UseCompatibleStateImageBehavior = false;
             this.lstProperties.View = System.Windows.Forms.View.Details;
@@ -247,16 +259,17 @@
             // dlgImageInfo
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(473, 443);
+            this.ClientSize = new System.Drawing.Size(591, 554);
             this.Controls.Add(this.lstProperties);
             this.Controls.Add(this.txtComment);
             this.Controls.Add(this.lblComment);
             this.Controls.Add(this.pnlBottom);
             this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "dlgImageInfo";
--- TotalImage/dlgImageInfo.cs
+++ TotalImage/dlgImageInfo.cs
@@ -18,20 +18,34 @@ namespace TotalImage
             lstProperties.Groups[2].CollapsedState = ListViewGroupCollapsedState.Expanded;
             lstProperties.Groups[3].CollapsedState = ListViewGroupCollapsedState.Expanded;
 
+            //Fixes the column width on high DPI screens
+            lstProperties.Columns[1].Width = lstProperties.ClientRectangle.Width - lstProperties.Columns[0].Width;
+
             frmMain mainForm = (frmMain)Application.OpenForms["frmMain"];
 
-            lstProperties.FindItemWithText("Filename").SubItems[1].Text = mainForm.filename;
-            lstProperties.FindItemWithText("File size").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.Length, true);
+            lstProperties.FindItemWithText("File name").SubItems[1].Text = mainForm.filename;
+            lstProperties.FindItemWithText("File size").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.Length, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
             lstProperties.FindItemWithText("Container type").SubItems[1].Text = mainForm.image.DisplayName;
             lstProperties.FindItemWithText("Container subtype").SubItems[1].Text = "N/A"; //Obtain this from the container metadata if it exists
             lstProperties.FindItemWithText("File system").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.DisplayName;
-            lstProperties.FindItemWithText("Volume label").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.VolumeLabel;
+            var volLabel = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.VolumeLabel;
+            System.Diagnostics.Debug.WriteLine(volLabel);
+            _ = string.IsNullOrWhiteSpace(volLabel) ? lstProperties.FindItemWithText("Volume label").SubItems[1].Text = "N/A" : lstProperties.FindItemWithText("Volume label").SubItems[1].Text = volLabel;
             lstProperties.FindItemWithText("Partitioning scheme").SubItems[1].Text = mainForm.image.PartitionTable.DisplayName;
             lstProperties.FindItemWithText("No. of partitions").SubItems[1].Text = mainForm.image.PartitionTable.Partitions.Count.ToString();
             lstProperties.FindItemWithText("Selected partition").SubItems[1].Text = mainForm.CurrentPartitionIndex.ToString();
+            lstProperties.FindItemWithText("Partition ID/type").SubItems[1].Text = "N/A"; //Obtain this from the partition entry, if it exists
+            lstProperties.FindItemWithText("Files").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.RootDirectory.FileCount(true).ToString();
+            lstProperties.FindItemWithText("Subdirectories").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.RootDirectory.SubdirectoryCount(true).ToString();        
             lstProperties.FindItemWithText("Created by").SubItems[1].Text = "N/A"; //Obtain this from the container metadata if it exists
-            lstProperties.FindItemWithText("Total storage capacity").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].Length);
-            lstProperties.FindItemWithText("Free space").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.TotalFreeSpace);
+            lstProperties.FindItemWithText("Total storage capacity").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].Length, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
+            lstProperties.FindItemWithText("Free space").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.TotalFreeSpace, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
+            lstProperties.FindItemWithText("Volume serial number").SubItems[1].Text = "N/A"; //Obtain this from the BPB if it exists
+
+            //Obtain this from the container metadata if it exists
+            txtComment.Text = "This container type does not support comments."; 
+            txtComment.Enabled = false;
+            lblComment.Enabled = false;
         }
     }
 }
