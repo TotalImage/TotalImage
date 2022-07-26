5d162e63565d6fdd9887940c6614d6f00a3a6692
David Simunič <simunic.david@outlook.com>
2022-07-26 19:42:53 +0200

    Changes to the image info fields
    Added several new fields and move some around
    Also applied these changes to the txt export code

--- TotalImage/dlgImageInfo.cs
+++ TotalImage/dlgImageInfo.cs
@@ -19,14 +19,20 @@ namespace TotalImage
         private void dlgImageInfo_Load(object sender, System.EventArgs e)
         {
             frmMain mainForm = (frmMain)Application.OpenForms["frmMain"];
+            FileInfo fileInfo = new FileInfo(mainForm.filepath);
 
             //Fixes the column width on high DPI screens
             this.lstProperties.Columns[1].Width = lstProperties.ClientRectangle.Width - lstProperties.Columns[0].Width;
 
-            lstProperties.FindItemWithText("File name").SubItems[1].Text = mainForm.filename;
-            lstProperties.FindItemWithText("File size").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.Length, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
+            lstProperties.FindItemWithText("Filename").SubItems[1].Text = mainForm.filename;
+            lstProperties.FindItemWithText("Size").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.Length, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
+            lstProperties.FindItemWithText("Created").SubItems[1].Text = fileInfo.CreationTime.ToString();
+            lstProperties.FindItemWithText("Modified").SubItems[1].Text = fileInfo.LastWriteTime.ToString();
+            lstProperties.FindItemWithText("Accessed").SubItems[1].Text = fileInfo.LastAccessTime.ToString();
+            lstProperties.FindItemWithText("Attributes").SubItems[1].Text = fileInfo.Attributes.ToString();
             lstProperties.FindItemWithText("Container type").SubItems[1].Text = mainForm.image.DisplayName;
             lstProperties.FindItemWithText("Container subtype").SubItems[1].Text = "N/A"; //Obtain this from the container metadata if it exists
+            lstProperties.FindItemWithText("Version").SubItems[1].Text = "N/A"; //Obtain this from the container metadata if it exists
             lstProperties.FindItemWithText("File system").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.DisplayName;
             var volLabel = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.VolumeLabel;
             _ = string.IsNullOrWhiteSpace(volLabel) ? lstProperties.FindItemWithText("Volume label").SubItems[1].Text = "N/A" : lstProperties.FindItemWithText("Volume label").SubItems[1].Text = volLabel;
@@ -36,7 +42,7 @@ namespace TotalImage
             lstProperties.FindItemWithText("Partition ID/type").SubItems[1].Text = "N/A"; //Obtain this from the partition entry, if it exists
             lstProperties.FindItemWithText("Files").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.RootDirectory.FileCount(true).ToString();
             lstProperties.FindItemWithText("Subdirectories").SubItems[1].Text = mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.RootDirectory.SubdirectoryCount(true).ToString();
-            lstProperties.FindItemWithText("Created by").SubItems[1].Text = "N/A"; //Obtain this from the container metadata if it exists
+            lstProperties.FindItemWithText("Created by").SubItems[1].Text = "Unknown"; //Obtain this from the container metadata if it exists
             lstProperties.FindItemWithText("Total storage capacity").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].Length, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
             lstProperties.FindItemWithText("Free space").SubItems[1].Text = Settings.CurrentSettings.SizeUnit.FormatSize((ulong)mainForm.image.PartitionTable.Partitions[mainForm.CurrentPartitionIndex].FileSystem.TotalFreeSpace, Settings.CurrentSettings.SizeUnit != SizeUnit.Bytes);
             lstProperties.FindItemWithText("Volume serial number").SubItems[1].Text = "N/A"; //Obtain this from the BPB if it exists
@@ -67,27 +73,13 @@ namespace TotalImage
                 sw.WriteLine("*** TotalImage image information ***");
                 sw.WriteLine($"Created on {DateTime.Now}");
                 sw.WriteLine();
-                sw.WriteLine("Container information");
-                sw.WriteLine($"-File name: {lstProperties.FindItemWithText("File name").SubItems[1].Text}");
-                sw.WriteLine($"-File size: {lstProperties.FindItemWithText("File size").SubItems[1].Text}");
-                sw.WriteLine($"-Container type: {lstProperties.FindItemWithText("Container type").SubItems[1].Text}");
-                sw.WriteLine($"-Container subtype: {lstProperties.FindItemWithText("Container subtype").SubItems[1].Text}");
-                sw.WriteLine($"-Created by: {lstProperties.FindItemWithText("Created by").SubItems[1].Text}");
-                sw.WriteLine();
-                sw.WriteLine("Partition information");
-                sw.WriteLine($"-Partitioning scheme: {lstProperties.FindItemWithText("Partitioning scheme").SubItems[1].Text}");
-                sw.WriteLine($"-No. of partitions: {lstProperties.FindItemWithText("No. of partitions").SubItems[1].Text}");
-                sw.WriteLine($"-Selected partition: {lstProperties.FindItemWithText("Selected partition").SubItems[1].Text}");
-                sw.WriteLine($"-Partition ID/type: {lstProperties.FindItemWithText("Partition ID/type").SubItems[1].Text}");
-                sw.WriteLine();
-                sw.WriteLine("File system information");
-                sw.WriteLine($"-File system: {lstProperties.FindItemWithText("File system").SubItems[1].Text}");
-                sw.WriteLine($"-Volume label: {lstProperties.FindItemWithText("Volume label").SubItems[1].Text}");
-                sw.WriteLine($"-Volume serial number: {lstProperties.FindItemWithText("Volume serial number").SubItems[1].Text}");
-                sw.WriteLine($"-Total storage capacity: {lstProperties.FindItemWithText("Total storage capacity").SubItems[1].Text}");
-                sw.WriteLine($"-Free space: {lstProperties.FindItemWithText("Free space").SubItems[1].Text}");
-                sw.WriteLine($"-Files: {lstProperties.FindItemWithText("Files").SubItems[1].Text}");
-                sw.WriteLine($"-Subdirectories: {lstProperties.FindItemWithText("Subdirectories").SubItems[1].Text}");
+                sw.WriteLine("File information");
+                sw.WriteLine($"-Filename: {lstProperties.FindItemWithText("Filename").SubItems[1].Text}");
+                sw.WriteLine($"-Size: {lstProperties.FindItemWithText("Size").SubItems[1].Text}");
+                sw.WriteLine($"-Created: {lstProperties.FindItemWithText("Created").SubItems[1].Text}");
+                sw.WriteLine($"-Modified: {lstProperties.FindItemWithText("Modified").SubItems[1].Text}");
+                sw.WriteLine($"-Accessed: {lstProperties.FindItemWithText("Accessed").SubItems[1].Text}");
+                sw.WriteLine($"-Attributes: {lstProperties.FindItemWithText("Attributes").SubItems[1].Text}");
 
                 //Skip the hashes if they're not done yet
                 if (!hashesDone)
@@ -107,12 +99,31 @@ namespace TotalImage
                 }
                 else
                 {
-                    sw.WriteLine();
-                    sw.WriteLine("Miscellaneous");
                     sw.WriteLine($"-MD5 hash: {lstProperties.FindItemWithText("MD5 hash").SubItems[1].Text}");
                     sw.WriteLine($"-SHA-1 hash: {lstProperties.FindItemWithText("SHA-1 hash").SubItems[1].Text}");
                 }
 
+                sw.WriteLine();
+                sw.WriteLine("Container information");
+                sw.WriteLine($"-Container type: {lstProperties.FindItemWithText("Container type").SubItems[1].Text}");
+                sw.WriteLine($"-Container subtype: {lstProperties.FindItemWithText("Container subtype").SubItems[1].Text}");
+                sw.WriteLine($"-Version: {lstProperties.FindItemWithText("Version").SubItems[1].Text}");
+                sw.WriteLine($"-Created by: {lstProperties.FindItemWithText("Created by").SubItems[1].Text}");
+                sw.WriteLine();
+                sw.WriteLine("Partition information");
+                sw.WriteLine($"-Partitioning scheme: {lstProperties.FindItemWithText("Partitioning scheme").SubItems[1].Text}");
+                sw.WriteLine($"-No. of partitions: {lstProperties.FindItemWithText("No. of partitions").SubItems[1].Text}");
+                sw.WriteLine($"-Selected partition: {lstProperties.FindItemWithText("Selected partition").SubItems[1].Text}");
+                sw.WriteLine($"-Partition ID/type: {lstProperties.FindItemWithText("Partition ID/type").SubItems[1].Text}");
+                sw.WriteLine();
+                sw.WriteLine("File system information");
+                sw.WriteLine($"-File system: {lstProperties.FindItemWithText("File system").SubItems[1].Text}");
+                sw.WriteLine($"-Volume label: {lstProperties.FindItemWithText("Volume label").SubItems[1].Text}");
+                sw.WriteLine($"-Volume serial number: {lstProperties.FindItemWithText("Volume serial number").SubItems[1].Text}");
+                sw.WriteLine($"-Total storage capacity: {lstProperties.FindItemWithText("Total storage capacity").SubItems[1].Text}");
+                sw.WriteLine($"-Free space: {lstProperties.FindItemWithText("Free space").SubItems[1].Text}");
+                sw.WriteLine($"-Files: {lstProperties.FindItemWithText("Files").SubItems[1].Text}");
+                sw.WriteLine($"-Subdirectories: {lstProperties.FindItemWithText("Subdirectories").SubItems[1].Text}");          
                 sw.WriteLine();
                 sw.Write("*** End of file ***");
                 sw.Flush();
