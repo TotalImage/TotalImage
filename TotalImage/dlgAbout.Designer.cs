42554e45efb8ee5bbd7374d2282ce4a84cf570e1
David Simunič <simunic.david@outlook.com>
2023-02-26 11:11:56 +0100

    Some minor tweaks on dialogs

--- TotalImage/dlgAbout.Designer.cs
+++ TotalImage/dlgAbout.Designer.cs
@@ -46,9 +46,10 @@
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnOK.Location = new System.Drawing.Point(209, 12);
+            this.btnOK.Location = new System.Drawing.Point(222, 15);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(80, 26);
+            this.btnOK.Size = new System.Drawing.Size(100, 32);
             this.btnOK.TabIndex = 0;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -57,36 +58,40 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblTitle.Location = new System.Drawing.Point(66, 12);
+            this.lblTitle.Location = new System.Drawing.Point(82, 15);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(96, 21);
+            this.lblTitle.Size = new System.Drawing.Size(117, 28);
             this.lblTitle.TabIndex = 1;
             this.lblTitle.Text = "TotalImage";
             // 
             // lblDesc
             // 
             this.lblDesc.AutoSize = true;
-            this.lblDesc.Location = new System.Drawing.Point(67, 43);
+            this.lblDesc.Location = new System.Drawing.Point(84, 54);
+            this.lblDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDesc.Name = "lblDesc";
-            this.lblDesc.Size = new System.Drawing.Size(143, 15);
+            this.lblDesc.Size = new System.Drawing.Size(183, 20);
             this.lblDesc.TabIndex = 2;
             this.lblDesc.Text = "A better disk image editor";
             // 
             // lblVer
             // 
             this.lblVer.AutoSize = true;
-            this.lblVer.Location = new System.Drawing.Point(9, 73);
+            this.lblVer.Location = new System.Drawing.Point(11, 91);
+            this.lblVer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVer.Name = "lblVer";
-            this.lblVer.Size = new System.Drawing.Size(118, 15);
+            this.lblVer.Size = new System.Drawing.Size(148, 20);
             this.lblVer.TabIndex = 3;
             this.lblVer.Text = "Version: 1.0.0.0 Alpha";
             // 
             // lblCopyright
             // 
             this.lblCopyright.AutoSize = true;
-            this.lblCopyright.Location = new System.Drawing.Point(9, 97);
+            this.lblCopyright.Location = new System.Drawing.Point(11, 121);
+            this.lblCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(244, 15);
+            this.lblCopyright.Size = new System.Drawing.Size(312, 20);
             this.lblCopyright.TabIndex = 4;
             this.lblCopyright.Text = "Copyright © 2020-2023 The TotalImage Team";
             // 
@@ -95,17 +100,19 @@
             this.pnlBottom.BackColor = System.Drawing.SystemColors.Control;
             this.pnlBottom.Controls.Add(this.btnOK);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 191);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 241);
+            this.pnlBottom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(301, 50);
+            this.pnlBottom.Size = new System.Drawing.Size(337, 62);
             this.pnlBottom.TabIndex = 5;
             // 
             // imgLogo
             // 
             this.imgLogo.Image = global::TotalImage.Properties.Resources.logo_48;
-            this.imgLogo.Location = new System.Drawing.Point(12, 12);
+            this.imgLogo.Location = new System.Drawing.Point(15, 15);
+            this.imgLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.imgLogo.Name = "imgLogo";
-            this.imgLogo.Size = new System.Drawing.Size(48, 48);
+            this.imgLogo.Size = new System.Drawing.Size(60, 60);
             this.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgLogo.TabIndex = 6;
             this.imgLogo.TabStop = false;
@@ -113,19 +120,21 @@
             // lblLicense
             // 
             this.lblLicense.AutoSize = true;
-            this.lblLicense.Location = new System.Drawing.Point(9, 121);
+            this.lblLicense.Location = new System.Drawing.Point(11, 151);
+            this.lblLicense.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLicense.Name = "lblLicense";
-            this.lblLicense.Size = new System.Drawing.Size(282, 30);
+            this.lblLicense.Size = new System.Drawing.Size(304, 40);
             this.lblLicense.TabIndex = 7;
-            this.lblLicense.Text = "TotalImage is licensed under the MIT license. See the\r\nLICENSE file for more info" +
+            this.lblLicense.Text = "TotalImage is licensed under the MIT license.\r\nSee the LICENSE file for more info" +
     "rmation.";
             // 
             // lnkGitHub
             // 
             this.lnkGitHub.AutoSize = true;
-            this.lnkGitHub.Location = new System.Drawing.Point(9, 160);
+            this.lnkGitHub.Location = new System.Drawing.Point(11, 200);
+            this.lnkGitHub.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lnkGitHub.Name = "lnkGitHub";
-            this.lnkGitHub.Size = new System.Drawing.Size(235, 15);
+            this.lnkGitHub.Size = new System.Drawing.Size(294, 20);
             this.lnkGitHub.TabIndex = 8;
             this.lnkGitHub.TabStop = true;
             this.lnkGitHub.Text = "https://github.com/TotalImage/TotalImage";
@@ -134,10 +143,10 @@
             // dlgAbout
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(301, 241);
+            this.ClientSize = new System.Drawing.Size(337, 303);
             this.Controls.Add(this.lnkGitHub);
             this.Controls.Add(this.lblLicense);
             this.Controls.Add(this.imgLogo);
@@ -147,6 +156,7 @@
             this.Controls.Add(this.lblDesc);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "dlgAbout";
--- TotalImage/dlgExtract.Designer.cs
+++ TotalImage/dlgExtract.Designer.cs
@@ -46,8 +46,8 @@
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnOK.Location = new System.Drawing.Point(576, 15);
-            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOK.Location = new System.Drawing.Point(494, 15);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(100, 32);
             this.btnOK.TabIndex = 6;
@@ -60,8 +60,8 @@
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnCancel.Location = new System.Drawing.Point(684, 15);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Location = new System.Drawing.Point(602, 15);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(100, 32);
             this.btnCancel.TabIndex = 7;
@@ -71,7 +71,7 @@
             // lblPath
             // 
             this.lblPath.AutoSize = true;
-            this.lblPath.Location = new System.Drawing.Point(11, 11);
+            this.lblPath.Location = new System.Drawing.Point(11, 21);
             this.lblPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPath.Name = "lblPath";
             this.lblPath.Size = new System.Drawing.Size(320, 20);
@@ -81,17 +81,17 @@
             // txtPath
             // 
             this.txtPath.Location = new System.Drawing.Point(15, 45);
-            this.txtPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(4);
             this.txtPath.Name = "txtPath";
-            this.txtPath.Size = new System.Drawing.Size(660, 27);
+            this.txtPath.Size = new System.Drawing.Size(579, 27);
             this.txtPath.TabIndex = 0;
             // 
             // cbxOpenFolder
             // 
             this.cbxOpenFolder.AutoSize = true;
             this.cbxOpenFolder.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.cbxOpenFolder.Location = new System.Drawing.Point(15, 136);
-            this.cbxOpenFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxOpenFolder.Location = new System.Drawing.Point(15, 125);
+            this.cbxOpenFolder.Margin = new System.Windows.Forms.Padding(4);
             this.cbxOpenFolder.Name = "cbxOpenFolder";
             this.cbxOpenFolder.Size = new System.Drawing.Size(303, 25);
             this.cbxOpenFolder.TabIndex = 5;
@@ -104,7 +104,7 @@
             this.rbnIgnoreFolders.Checked = true;
             this.rbnIgnoreFolders.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.rbnIgnoreFolders.Location = new System.Drawing.Point(15, 92);
-            this.rbnIgnoreFolders.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbnIgnoreFolders.Margin = new System.Windows.Forms.Padding(4);
             this.rbnIgnoreFolders.Name = "rbnIgnoreFolders";
             this.rbnIgnoreFolders.Size = new System.Drawing.Size(132, 25);
             this.rbnIgnoreFolders.TabIndex = 2;
@@ -117,7 +117,7 @@
             this.rbnExtractSameFolder.AutoSize = true;
             this.rbnExtractSameFolder.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.rbnExtractSameFolder.Location = new System.Drawing.Point(152, 92);
-            this.rbnExtractSameFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbnExtractSameFolder.Margin = new System.Windows.Forms.Padding(4);
             this.rbnExtractSameFolder.Name = "rbnExtractSameFolder";
             this.rbnExtractSameFolder.Size = new System.Drawing.Size(273, 25);
             this.rbnExtractSameFolder.TabIndex = 3;
@@ -130,7 +130,7 @@
             this.rbnPreserveDirs.AutoSize = true;
             this.rbnPreserveDirs.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.rbnPreserveDirs.Location = new System.Drawing.Point(429, 92);
-            this.rbnPreserveDirs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbnPreserveDirs.Margin = new System.Windows.Forms.Padding(4);
             this.rbnPreserveDirs.Name = "rbnPreserveDirs";
             this.rbnPreserveDirs.Size = new System.Drawing.Size(273, 25);
             this.rbnPreserveDirs.TabIndex = 4;
@@ -140,8 +140,8 @@
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(684, 44);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowse.Location = new System.Drawing.Point(602, 42);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(100, 32);
             this.btnBrowse.TabIndex = 1;
@@ -155,10 +155,10 @@
             this.pnlBottom.Controls.Add(this.btnCancel);
             this.pnlBottom.Controls.Add(this.btnOK);
             this.pnlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.pnlBottom.Location = new System.Drawing.Point(0, 180);
-            this.pnlBottom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 166);
+            this.pnlBottom.Margin = new System.Windows.Forms.Padding(4);
             this.pnlBottom.Name = "pnlBottom";
-            this.pnlBottom.Size = new System.Drawing.Size(799, 62);
+            this.pnlBottom.Size = new System.Drawing.Size(717, 62);
             this.pnlBottom.TabIndex = 11;
             // 
             // dlgExtract
@@ -168,7 +168,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(799, 242);
+            this.ClientSize = new System.Drawing.Size(717, 228);
             this.Controls.Add(this.pnlBottom);
             this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.rbnPreserveDirs);
@@ -178,7 +178,7 @@
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.lblPath);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "dlgExtract";
