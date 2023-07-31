6c5813024100f6bd641ed2cfe440a78f7f57b557
David Simunič <simunic.david@outlook.com>
2023-07-31 21:32:51 +0200

    Optimized ISO and HSF detection
    Removed needless additional seeking and reading in the High Sierra block - both ISO and HSF check is now done at the same time with a single seek and read per loop iteration
    Also reduced the loop ceiling to 512 KiB

--- TotalImage.IO/FileSystems/ISO/IsoFactory.cs
+++ TotalImage.IO/FileSystems/ISO/IsoFactory.cs
@@ -13,46 +13,30 @@ namespace TotalImage.FileSystems.ISO
         public FileSystem? TryLoadFileSystem(Stream stream)
         {
             long nextOffset = 0x8001;
-            byte[] identifier = new byte[5];
+            byte[] identifierBytes = new byte[13];
 
+            //Although the primary volume descriptor must be located at offset 0x8000 (sector 17, counting from 1), there are images out there (bad dumps?)
+            //where it's further into the file, so we try to catch such cases with this loop by looking for the descriptor's identifier for a bit before
+            //giving up at 512 KiB. We might also want to eventually check the volume descriptor ID at this step already - should be 0x01 in this case.
             do
             {
                 stream.Seek(nextOffset, SeekOrigin.Begin);
-                stream.Read(identifier);
+                stream.Read(identifierBytes);
 
-                //First check if this might be ISO 9660
-                if (identifier.SequenceEqual(IsoVolumeDescriptor.IsoStandardIdentifier))
+                //First check if this might be ISO 9660 or High Sierra
+                if (identifierBytes[0..5].SequenceEqual(IsoVolumeDescriptor.IsoStandardIdentifier) || identifierBytes[8..13].SequenceEqual(IsoVolumeDescriptor.HsfStandardIdentifier))
                 {
-                    // trim off any leading blocks in this image
-
+                    // trim off any leading blocks in this image - e.g. if there's more than 32 KiB of system area before the primary volume descriptor,
+                    // ditch them because that's out of spec and probably the result of a bad dump
                     long partialStreamStart = nextOffset - 0x8001;
                     return partialStreamStart == 0
                         ? new Iso9660FileSystem(stream)
                         : new Iso9660FileSystem(new PartialStream(stream, partialStreamStart, stream.Length - partialStreamStart));
                 }
-                else
-                {
-                    //If not, check for High Sierra instead
-                    nextOffset += 8;
-                    stream.Seek(nextOffset, SeekOrigin.Begin);
-                    stream.Read(identifier);
 
-                    if (identifier.SequenceEqual(IsoVolumeDescriptor.HsfStandardIdentifier))
-                    {
-                        long partialStreamStart = nextOffset - 0x8009;
-                        return partialStreamStart == 0
-                            ? new Iso9660FileSystem(stream)
-                            : new Iso9660FileSystem(new PartialStream(stream, partialStreamStart, stream.Length - partialStreamStart));
-                    }
-                    else
-                    {
-                        nextOffset -= 8;
-                    }
-                }
-
-                nextOffset += 0x800;
+                nextOffset += 0x800; //If it's neither, just seek to the next sector and try again
             }
-            while (nextOffset < stream.Length && nextOffset < 0x100000); //Stop at 1 MiB because it's effectively pointless to look further
+            while (nextOffset < stream.Length && nextOffset < 0x80000); //Finally, stop at 512 KiB because it's effectively pointless to look further
 
             return null;
         }
