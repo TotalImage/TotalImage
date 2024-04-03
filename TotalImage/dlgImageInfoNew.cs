34d5b10b9b69b8bc1d5ed9ebedd4a272a01e4407
David Simunič <simunic.david@outlook.com>
2024-04-03 19:01:37 +0200

    Refactored info export again to be more universal
    No longer depends on how the individual listviewitems are called, as it now loops over all the shown items for each listview

--- TotalImage/dlgImageInfoNew.cs
+++ TotalImage/dlgImageInfoNew.cs
@@ -188,7 +188,7 @@ namespace TotalImage
                 var hash = attrs.FirstOrDefault(a => a.Key == "GitHash");
 
                 //Note that aside from the "created on" datetime at the top, all datetime values are currently exported in the user's locale!
-                //Same for the decimal and thousands separator in numbers.
+                //Same for the decimal and thousands separator in numbers. Might be worth revisiting this at some point...
                 StreamWriter sw = File.CreateText(sfd.FileName);
                 sw.WriteLine("************************************");
                 sw.WriteLine("*            TotalImage            *");
@@ -197,45 +197,45 @@ namespace TotalImage
                 sw.WriteLine($"-TotalImage version: {asm.GetName().Version} ({hash.Value})");
                 sw.WriteLine($"-Created on: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}");
                 sw.WriteLine();
+
+                //Iterate over all the displayed listview items for each listview
                 sw.WriteLine("** File information **");
-                sw.WriteLine($"-Filename: {lstPropertiesFile.FindItemWithText("Filename").SubItems[1].Text}");
-                sw.WriteLine($"-Size: {lstPropertiesFile.FindItemWithText("Size").SubItems[1].Text}");
-                sw.WriteLine($"-Created: {lstPropertiesFile.FindItemWithText("Created").SubItems[1].Text}");
-                sw.WriteLine($"-Modified: {lstPropertiesFile.FindItemWithText("Modified").SubItems[1].Text}");
-                sw.WriteLine($"-Accessed: {lstPropertiesFile.FindItemWithText("Accessed").SubItems[1].Text}");
-                sw.WriteLine($"-Attributes: {lstPropertiesFile.FindItemWithText("Attributes").SubItems[1].Text}");
-
-                //Skip the file hashes if they're not done yet
-                if (hashesDone)
+                foreach(ListViewItem lvi in lstPropertiesFile.Items)
                 {
-                    sw.WriteLine($"-MD5 hash: {lstPropertiesFile.FindItemWithText("MD5 hash").SubItems[1].Text}");
-                    sw.WriteLine($"-SHA-1 hash: {lstPropertiesFile.FindItemWithText("SHA-1 hash").SubItems[1].Text}");
-                }
+                    //Skip the file hashes if they're not done yet
+                    if (!hashesDone && (lvi.Text == "MD5 hash" || lvi.Text == "SHA-1 hash"))
+                        continue;
 
+                    sw.WriteLine($"-{lvi.Text}: {lvi.SubItems[1].Text}");
+                }
                 sw.WriteLine();
+
                 sw.WriteLine("** Container information **");
-                sw.WriteLine($"-Container type: {lstPropertiesContainer.FindItemWithText("Container type").SubItems[1].Text}");
-                sw.WriteLine($"-Container subtype: {lstPropertiesContainer.FindItemWithText("Container subtype").SubItems[1].Text}");
-                sw.WriteLine($"-Container version: {lstPropertiesContainer.FindItemWithText("Container version").SubItems[1].Text}");
-                sw.WriteLine($"-Created by: {lstPropertiesContainer.FindItemWithText("Created by").SubItems[1].Text}");
-                sw.WriteLine($"-Creator version: {lstPropertiesContainer.FindItemWithText("Creator version").SubItems[1].Text}");
+                foreach (ListViewItem lvi in lstPropertiesContainer.Items)
+                {
+                    sw.WriteLine($"-{lvi.Text}: {lvi.SubItems[1].Text}");
+                }
                 sw.WriteLine();
+
                 sw.WriteLine("** Partition table information **");
-                sw.WriteLine($"-Partitioning scheme: {lstPropertiesPT.FindItemWithText("Partitioning scheme").SubItems[1].Text}");
-                sw.WriteLine($"-No. of partitions: {lstPropertiesPT.FindItemWithText("No. of partitions").SubItems[1].Text}");
-                sw.WriteLine($"-Selected partition: {lstPropertiesPT.FindItemWithText("Selected partition").SubItems[1].Text}");
+                foreach (ListViewItem lvi in lstPropertiesPT.Items)
+                {
+                    sw.WriteLine($"-{lvi.Text}: {lvi.SubItems[1].Text}");
+                }
                 sw.WriteLine();
+
                 sw.WriteLine("** Selected partition information **");
-                sw.WriteLine($"-Partition ID/type: {lstPropertiesPartition.FindItemWithText("Partition ID/type").SubItems[1].Text}");
+                foreach (ListViewItem lvi in lstPropertiesPartition.Items)
+                {
+                    sw.WriteLine($"-{lvi.Text}: {lvi.SubItems[1].Text}");
+                }
                 sw.WriteLine();
+
                 sw.WriteLine("** File system information **");
-                sw.WriteLine($"-File system: {lstPropertiesFS.FindItemWithText("File system").SubItems[1].Text}");
-                sw.WriteLine($"-Volume label: {lstPropertiesFS.FindItemWithText("Volume label").SubItems[1].Text}");
-                sw.WriteLine($"-Volume serial number: {lstPropertiesFS.FindItemWithText("Volume serial number").SubItems[1].Text}");
-                sw.WriteLine($"-Total storage capacity: {lstPropertiesFS.FindItemWithText("Total storage capacity").SubItems[1].Text}");
-                sw.WriteLine($"-Free space: {lstPropertiesFS.FindItemWithText("Free space").SubItems[1].Text}");
-                sw.WriteLine($"-Files: {lstPropertiesFS.FindItemWithText("Files").SubItems[1].Text}");
-                sw.WriteLine($"-Subdirectories: {lstPropertiesFS.FindItemWithText("Subdirectories").SubItems[1].Text}");
+                foreach (ListViewItem lvi in lstPropertiesFS.Items)
+                {
+                    sw.WriteLine($"-{lvi.Text}: {lvi.SubItems[1].Text}");
+                }
                 sw.WriteLine();
 
                 if (mainForm.image is IContainerComment)
