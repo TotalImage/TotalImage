f458344ca5ebff37bcc0ff7b7dc5169194281e96
David Simunič <simunic.david@outlook.com>
2024-11-01 21:49:24 +0100

    Initial work on the Operations framework
    This will be used to track changes made to the image.

---
+++ TotalImage.IO/Operations/OemIdChangedOperation.cs
@@ -0,0 +1,23 @@
+using TotalImage.Partitions;
+
+namespace TotalImage.Operations
+{
+    /// <summary>
+    /// An operation for changing the OEM ID of a boot sector.
+    /// </summary>
+    /// <param name="targetObject">The partition whose OEM ID to be changed.</param>
+    /// <param name="oldOemId">Old OEM ID.</param>
+    /// <param name="newOemId">New OEM ID.</param>
+    public class OemIdChangedOperation(PartitionEntry targetObject, string oldOemId, string newOemId) : Operation(targetObject)
+    {
+        /// <summary>
+        /// The OEM ID before this operation.
+        /// </summary>
+        public string OldOemId { get; } = oldOemId;
+
+        /// <summary>
+        /// The OEM ID after this operation.
+        /// </summary>
+        public string NewOemId { get; } = newOemId;
+    }
+}
