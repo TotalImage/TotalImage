ba01563b9bb216f92c5a578e1a4d82881c2b3c14
David Simunič <simunic.david@outlook.com>
2025-09-09 18:30:25 +0200

    More Operations works
    Added a check for FileSystem.SupportsSubdirectories to DirectoryCreatedOperation
    Added a check if Container is IContainerComment for ImageCommentChangedOperation
    Added a check if read-only attribute isn't being cleared for ObjectAttributesChangedOperation

--- TotalImage.IO/Operations/ObjectAttributesChangedOperation.cs
+++ TotalImage.IO/Operations/ObjectAttributesChangedOperation.cs
@@ -7,19 +7,36 @@ namespace TotalImage.Operations
     /// <summary>
     /// An operation for changing the attributes of a file system object.
     /// </summary>
-    /// <param name="targetObject">The file system object whose attributes are to be changed.</param>
-    /// <param name="oldAttributes">Old attributes of the file system object.</param>
-    /// <param name="newAttributes">New attributes of the file system object.</param>
-    /// <param name="timestamp">The date and time when the file system object attributes were changed.</param>
-    public class ObjectAttributesChangedOperation(TiFileSystemObject targetObject, FileAttributes oldAttributes, FileAttributes newAttributes, DateTime timestamp) : Operation(targetObject, timestamp)
+
+    public class ObjectAttributesChangedOperation : Operation
     {
         /// <summary>
         /// The attributes of the target object before this operation.
         /// </summary>
-        public FileAttributes OldAttributes { get; } = oldAttributes;
+        public FileAttributes OldAttributes { get; }
         /// <summary>
         /// The attributes of the target object after this operation.
         /// </summary>
-        public FileAttributes NewAttributes { get; } = newAttributes;
+        public FileAttributes NewAttributes { get; }
+
+        /// <summary>
+        /// Creates a new ObjectAttributesChangedOperation, if oldAttributes doesn't include read-only or if newAttributes clears it.
+        /// </summary>
+        /// <param name="targetObject">The file system object whose attributes are to be changed.</param>
+        /// <param name="oldAttributes">Old attributes of the file system object.</param>
+        /// <param name="newAttributes">New attributes of the file system object.</param>
+        /// <param name="timestamp">The date and time when the file system object attributes were changed.</param>
+        public ObjectAttributesChangedOperation(TiFileSystemObject targetObject, FileAttributes oldAttributes, FileAttributes newAttributes, DateTime timestamp) : base(targetObject, timestamp)
+        {
+            //If the old attributes have the read-only flag set and the new attributes don't clear it, the operation should fail since the only thing
+            //that can be changed on a read-only object is the read-only flag...
+            if(oldAttributes.HasFlag(FileAttributes.ReadOnly) && NewAttributes.HasFlag(FileAttributes.ReadOnly))
+            {
+                throw new InvalidOperationException("Cannot change attributes of a read-only object unless the read-only attribute is cleared first!");
+            }
+
+            OldAttributes = oldAttributes;
+            NewAttributes = newAttributes;
+        }
     }
 }
