5ecba69d32a7afeb329f06dd6cc2c4cdbafe3aed
David Simunič <simunic.david@outlook.com>
2025-09-09 18:13:07 +0200

    BootSectorChangedOperation tweaks
    Added oldBootSector argument
    Fixed capitalization in class and variable names
    Updated XML comments

--- TotalImage.IO/Operations/BootsectorReplacedOperation.cs
+++ TotalImage.IO/Operations/BootsectorReplacedOperation.cs
@@ -4,16 +4,22 @@ using TotalImage.Partitions;
 namespace TotalImage.Operations
 {
     /// <summary>
-    /// An operation for replacing the bootsector of a partition.
+    /// An operation for replacing the boot sector (MBR or VBR) of a partition or disk.
     /// </summary>
-    /// <param name="targetObject">The partition whose bootsector will be replaced.</param>
-    /// <param name="bootsector">The bootsector to be added to the partition.</param>
-    /// <param name="timestamp">The date and time when the bootsector was replaced.</param>
-    public class BootsectorReplacedOperation(PartitionEntry targetObject, byte[] bootsector, DateTime timestamp) : Operation(targetObject, timestamp)
+    /// <param name="targetObject">The partition whose boot sector will be replaced.</param>
+    /// <param name="oldBootSector">The current boot sector of the partition or disk.</param>
+    /// <param name="newBootSector">The boot sector to be written to the partition or disk.</param>
+    /// <param name="timestamp">The date and time when the boot sector was replaced.</param>
+    public class BootSectorReplacedOperation(PartitionEntry targetObject, byte[] oldBootSector, byte[] newBootSector, DateTime timestamp) : Operation(targetObject, timestamp)
     {
         /// <summary>
-        /// The bootsector to be added to the partition.
+        /// The current boot sector of the partition or disk.
         /// </summary>
-        public byte[] Bootsector { get; } = bootsector;
+        public byte[] OldBootSector { get; } = oldBootSector;
+
+        /// <summary>
+        /// The boot sector to be written to the partition or disk.
+        /// </summary>
+        public byte[] NewBootSector { get; } = newBootSector;
     }
 }
