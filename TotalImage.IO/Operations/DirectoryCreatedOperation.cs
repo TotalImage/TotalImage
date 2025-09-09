4eaa4c5ead9a3f81a2d7b8cf31f2ac84ce9a53ce
David Simunič <simunic.david@outlook.com>
2025-09-09 18:30:25 +0200

    More Operations works
    Added a check for FileSystem.SupportsSubdirectories to DirectoryCreatedOperation
    Added a check if Container is IContainerComment for ImageCommentChangedOperation
    Added a check if read-only attribute isn't being cleared for ObjectAttributesChangedOperation

--- TotalImage.IO/Operations/DirectoryCreatedOperation.cs
+++ TotalImage.IO/Operations/DirectoryCreatedOperation.cs
@@ -4,16 +4,31 @@ using TiDirectory = TotalImage.FileSystems.Directory;
 namespace TotalImage.Operations
 {
     /// <summary>
-    /// An operation for creating a new directory.
+    /// An operation for creating a new directory, if the file system supports subdirectories.
     /// </summary>
-    /// <param name="targetObject">The parent directory of the directory to be created.</param>
-    /// <param name="dirName">The name of the directory to be created.</param>
-    /// <param name="timestamp">The date and time when the directory was created.</param>
-    public class DirectoryCreatedOperation(TiDirectory targetObject, string dirName, DateTime timestamp) : Operation(targetObject, timestamp)
+
+    public class DirectoryCreatedOperation : Operation
     {
         /// <summary>
         /// The name of the directory that was created.
         /// </summary>
-        public string DirectoryName { get; } = dirName;
+        public string DirectoryName { get; }
+
+        /// <summary>
+        /// Creates a new DirectoryCreatedOperation with the provided arguments, if the current file system supports subdirectories.
+        /// </summary>
+        /// <param name="targetObject">The parent directory of the directory to be created.</param>
+        /// <param name="dirName">The name of the directory to be created.</param>
+        /// <param name="timestamp">The date and time when the directory was created.</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public DirectoryCreatedOperation(TiDirectory targetObject, string dirName, DateTime timestamp) : base(targetObject, timestamp)
+        {
+            if (!targetObject.FileSystem.SupportsSubdirectories)
+            {
+                throw new InvalidOperationException("This file system does not support subdirectories!");
+            }
+
+            DirectoryName = dirName;
+        }
     }
 }
--- TotalImage.IO/Operations/ImageCommentChangedOperation.cs
+++ TotalImage.IO/Operations/ImageCommentChangedOperation.cs
@@ -1,25 +1,40 @@
 using System;
 using TotalImage.Containers;
 
-namespace TotalImage.Operations
+namespace TotalImage.Operations 
 {
     /// <summary>
-    /// An operation for changing the comment of an image.
+    /// An operation for changing the comment of an image, if the container type supports comments.
     /// </summary>
-    /// <param name="targetObject">The image whose comment will be changed.</param>
-    /// <param name="oldComment">Old comment of the image.</param>
-    /// <param name="newComment">New comment of the image.</param>
-    /// <param name="timestamp">The date and time when the comment was changed.</param>
-    public class ImageCommentChangedOperation(Container targetObject, string oldComment, string newComment, DateTime timestamp) : Operation(targetObject, timestamp)
+
+    public class ImageCommentChangedOperation : Operation
     {
         /// <summary>
         /// The image comment before this operation.
         /// </summary>
-        public string OldComment { get; } = oldComment;
+        public string OldComment { get; }
 
         /// <summary>
         /// The image comment after this operation.
         /// </summary>
-        public string NewComment { get; } = newComment;
+        public string NewComment { get; }
+
+        /// <summary>
+        /// Creates a new ImageCommentChangedOperation, if the current container supports comments.
+        /// </summary>
+        /// <param name="targetObject">The image whose comment will be changed.</param>
+        /// <param name="oldComment">Old comment of the image.</param>
+        /// <param name="newComment">New comment of the image.</param>
+        /// <param name="timestamp">The date and time when the comment was changed.</param>
+        public ImageCommentChangedOperation(Container targetObject, string oldComment, string newComment, DateTime timestamp) : base(targetObject, timestamp)
+        {
+            if(targetObject is not IContainerComment)
+            {
+                throw new InvalidOperationException("This container type does not support comments!");
+            }
+
+            OldComment = oldComment;
+            NewComment = newComment;
+        }
     }
 }
