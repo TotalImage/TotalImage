ce65d6ab8a10a0ef474c1fa8bf0cfe7ba9412e81
David Simunič <simunic.david@outlook.com>
2025-09-09 19:23:15 +0200

    Safety checks. for Operations
    Null checks for targetObject and other arguments in various operations
    Check if oldParentDir and newParentDir in ObjectMovedOperation are the same
    Directory name check in DirectoryCreatedOperation
    Made FirstChar nullable in ObjectUndeletedOperation, since that's a FAT specific thing
    Also other safety checks...

--- TotalImage.IO/Operations/ObjectMovedOperation.cs
+++ TotalImage.IO/Operations/ObjectMovedOperation.cs
@@ -6,20 +6,38 @@ namespace TotalImage.Operations
     /// <summary>
     /// An operation for moving a file system object within the image.
     /// </summary>
-    /// <param name="targetObject">The file system object to be moved.</param>
-    /// <param name="oldParentDir">Old parent directory of the object to be moved.</param>
-    /// <param name="newParentDir">New parent directory of the object to be moved.</param>
-    /// <param name="timestamp">The date and time when the file system object was moved.</param>
-    public class ObjectMovedOperation(FileSystems.FileSystemObject targetObject, TiDirectory oldParentDir, TiDirectory newParentDir, DateTime timestamp) : Operation(targetObject, timestamp)
+    public class ObjectMovedOperation : Operation
     {
         /// <summary>
         /// The target object's parent directory before this operation.
         /// </summary>
-        public TiDirectory OldParentDirectory { get; } = oldParentDir;
+        public TiDirectory OldParentDirectory { get; }
 
         /// <summary>
         /// The target object's parent directory after this operation.
         /// </summary>
-        public TiDirectory NewParentDirectory { get; } = newParentDir;
+        public TiDirectory NewParentDirectory { get; }
+
+        /// <summary>
+        /// An operation for moving a file system object within the image.
+        /// </summary>
+        /// <param name="targetObject">The file system object to be moved.</param>
+        /// <param name="oldParentDir">Old parent directory of the object to be moved.</param>
+        /// <param name="newParentDir">New parent directory of the object to be moved.</param>
+        /// <param name="timestamp">The date and time when the file system object was moved.</param>
+        public ObjectMovedOperation(FileSystems.FileSystemObject targetObject, TiDirectory oldParentDir, TiDirectory newParentDir, DateTime timestamp) : base(targetObject, timestamp)
+        {
+            ArgumentNullException.ThrowIfNull(oldParentDir);
+            ArgumentNullException.ThrowIfNull(newParentDir);
+            
+            //Cannot move to the same directory it's already in...
+            if(oldParentDir.Equals(newParentDir))
+            {
+                throw new ArgumentException("newParentDir cannot be the same as oldParentDir!");
+            }
+
+            OldParentDirectory = oldParentDir;
+            NewParentDirectory = newParentDir;
+        }
     }
 }
--- TotalImage.IO/Operations/PartitionResizedOperation.cs
+++ TotalImage.IO/Operations/PartitionResizedOperation.cs
@@ -6,20 +6,40 @@ namespace TotalImage.Operations
     /// <summary>
     /// An operation for resizing a partition in the image.
     /// </summary>
-    /// <param name="targetObject">The partition to be resized.</param>
-    /// <param name="oldSize">Old size of the partition.</param>
-    /// <param name="newSize">New size of the partition.</param>
-    /// <param name="timestamp">The date and time when the partition was resized.</param>
-    public class PartitionResizedOperation(PartitionEntry targetObject, long oldSize, long newSize, DateTime timestamp) : Operation(targetObject, timestamp) 
+    public class PartitionResizedOperation : Operation
     {
         /// <summary>
         /// The old size of this partition before the operation.
         /// </summary>
-        public long OldSize { get;  } = oldSize;
+        public long OldSize { get; }
 
         /// <summary>
         /// The new size of this partition after the operation.
         /// </summary>
-        public long NewSize { get; } = newSize;
+        public long NewSize { get; }
+
+        /// <summary>
+        /// An operation for resizing a partition in the image.
+        /// </summary>
+        /// <param name="targetObject">The partition to be resized.</param>
+        /// <param name="oldSize">Old size of the partition.</param>
+        /// <param name="newSize">New size of the partition.</param>
+        /// <param name="timestamp">The date and time when the partition was resized.</param>
+        public PartitionResizedOperation(PartitionEntry targetObject, long oldSize, long newSize, DateTime timestamp) : base(targetObject, timestamp)
+        {
+            //Either size being 0 is indicative of a problem and not something we can work with in this operation...
+            if (oldSize <= 0)
+            {
+                throw new ArgumentException("Old size cannot be 0 or less!", nameof(oldSize));
+            }
+
+            if (newSize <= 0)
+            {
+                throw new ArgumentException("New size cannot be 0 or less!", nameof(newSize));
+            }
+
+            OldSize = oldSize;
+            NewSize = newSize;
+        }
     }
 }
