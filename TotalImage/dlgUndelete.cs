43b031a81b431fc7a88b641eadb1c298ee7ec7e7
David Simunič <simunic.david@outlook.com>
2025-09-09 18:16:31 +0200

    Fixed my oversight when removing old Undelete dialog UI...

--- TotalImage/dlgUndelete.cs
+++ TotalImage/dlgUndelete.cs
@@ -5,17 +5,13 @@ namespace TotalImage
 {
     public partial class dlgUndelete : Form
     {
-        public char FirstChar { get; private set; }
-
         public dlgUndelete()
         {
             InitializeComponent();
         }
-
-        //TODO: Perform character/full name validation
         private void btnOK_Click(object sender, EventArgs e)
         {
-            FirstChar = txtFirstChar.Text[0];
+
         }
     }
 }
