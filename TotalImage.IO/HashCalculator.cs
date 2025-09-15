a6b0c8d3abc279245bf1cdec6d469197143d248f
David Simunič <simunic.david@outlook.com>
2025-09-15 20:26:46 +0200

    Some cleaning and tweaking
    Removed unused leftover methods for MD5 and SHA-1
    Removed unused usings in HashCalculator
    Updated System.Linq.Async and System.Collections.Immutable packages

--- TotalImage.IO/HashCalculator.cs
+++ TotalImage.IO/HashCalculator.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,9 +11,9 @@ namespace TotalImage
     /// </summary>
     public static class HashCalculator
     {
-        static SemaphoreSlim hashMutex = new SemaphoreSlim(1, 1);
+        private static SemaphoreSlim hashMutex = new SemaphoreSlim(1, 1);
 
-        static async Task<string> CalculateHashAsyncCore(Stream stream, HashAlgorithm algorithm, CancellationToken cancellationToken)
+        private static async Task<string> CalculateHashAsyncCore(Stream stream, HashAlgorithm algorithm, CancellationToken cancellationToken)
         {
             await hashMutex.WaitAsync(cancellationToken);
             byte[] hash;
@@ -35,40 +32,16 @@ namespace TotalImage
         }
 
         /// <summary>
-        /// Calculates the MD5 hash of this file
+        /// Asynchronously calculates the MD5 hash of the provided stream.
         /// </summary>
-        /// <returns>A string containing the hexadecimal representation of the MD5 hash</returns>
-        public static string CalculateMd5Hash(Stream stream)
-        {
-            using MD5 md5 = MD5.Create();
-            stream.Seek(0, SeekOrigin.Begin);
-            byte[]? hash = md5.ComputeHash(stream);
-            return BitConverter.ToString(hash).Replace("-", "").ToLower();
-        }
-
-        /// <summary>
-        /// Asynchronously calculates the MD5 hash of this file
-        /// </summary>
-        /// <returns>A task that represents the asynchronous hash calculation operation and wraps the string containing the hexadecimal representation of the MD5 hash</returns>
+        /// <returns>A task that represents the asynchronous hash calculation operation and wraps the string containing the hexadecimal representation of the MD5 hash.</returns>
         public static async Task<string> CalculateMd5HashAsync(Stream stream, CancellationToken cancellationToken = default)
             => await CalculateHashAsyncCore(stream, MD5.Create(), cancellationToken);
 
         /// <summary>
-        /// Calculates the SHA-1 hash of this file
+        /// Asynchronously calculates the SHA-1 hash of the provided stream.
         /// </summary>
-        /// <returns>A string containing the hexadecimal representation of the SHA-1 hash</returns>
-        public static string CalculateSha1Hash(Stream stream)
-        {
-            using SHA1 sha1 = SHA1.Create();
-            stream.Seek(0, SeekOrigin.Begin);
-            byte[]? hash = sha1.ComputeHash(stream);
-            return BitConverter.ToString(hash).Replace("-", "").ToLower();
-        }
-
-        /// <summary>
-        /// Asynchronously calculates the SHA-1 hash of this file
-        /// </summary>
-        /// <returns>A task that represents the asynchronous hash calculation operation and wraps the string containing the hexadecimal representation of the SHA-1 hash</returns>
+        /// <returns>A task that represents the asynchronous hash calculation operation and wraps the string containing the hexadecimal representation of the SHA-1 hash.</returns>
         public static async Task<string> CalculateSha1HashAsync(Stream stream, CancellationToken cancellationToken = default)
             => await CalculateHashAsyncCore(stream,SHA1.Create(), cancellationToken);
     }
