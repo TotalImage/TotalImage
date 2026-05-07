8b8be3c8075f4ece44241be8e4baf2f91ab05fd2
daviunic <33632722+daviunic@users.noreply.github.com>
2026-05-07 18:47:33 +0200

    Fixed lstColorMode not being themed according to ColorMode setting
    Apparently this is a WinForms limitation...

--- TotalImage/dlgSettings.cs
+++ TotalImage/dlgSettings.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
+using static Interop.UxTheme;
 
 namespace TotalImage
 {
@@ -71,6 +72,11 @@ namespace TotalImage
         private void dlgSettings_Load(object sender, System.EventArgs e)
         {
             SyncUIWithSettings();
+
+            /* lstColorMode's Win32 handle is created eagerly because it lives on the initially-visible
+             * tab, so it misses the dark mode theming that is applied lazily to handles on other tabs.
+             * Explicitly set the theme based on the active color mode to work around this. */
+            SetWindowTheme(lstColorMode.Handle, Application.ColorMode == SystemColorMode.Dark || Application.IsDarkModeEnabled ? "DarkMode_CFD" : "", null);
         }
 
         //Syncs the dialog UI with CurrentSettings
